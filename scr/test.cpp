13f681c067bd23bbfb05a615a19c1ff26eeae59a
Julian <julian.schurhammer@gmail.com>
2016-04-21 12:35:58 +1200

    Fixed test program

--- scr/test.cpp
+++ scr/test.cpp
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <time.h>
 
-extern "C" int init_hardware();
+extern "C" int init(int d_level);
 extern "C" int write_digital(int chan, char level);
 extern "C" int read_analog(int chan);
 extern "C" int sleep(int sec, int usec);
-extern "C" int set_motor(int motor, int dir, int value);
+extern "C" int set_motor(int motor, int value);
 
 using namespace std;
 
 int main()
 {
-    init_hardware();
-    int power = 255;
-    set_motor(1, 1, power);
-    set_motor(2, 1, power);
+    init(0);
+    int power = 100;
+    set_motor(1, power);
+    set_motor(2, power);
     sleep(3, 0);
-    set_motor(1, 1, 0);
-    set_motor(2, 1, 0);
     return 0;
 }
 
