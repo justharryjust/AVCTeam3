03ebc6379ec1665a576060da36b414761030f631
Thomas Edwards <tomo12321@hotmail.com>
2016-05-05 18:29:42 +1200

    when reversing, it turns slightly
    updated code so that when it moves backwards it turns slightly. This should prevent it from getting stuck

--- scr/tom camera test.cpp
+++ scr/tom camera test.cpp
@@ -14,14 +14,14 @@ using namespace std;
 int main()
 {
   init(0);
+  bool flip = false;
         open_screen_stream();
         int pixels [320];
         while (true) {
             take_picture();
             update_screen();
-            //getting the halfway pixel (120) possibly change to top pixel?
             for (int i = 0;i<320;i++) {
-                int whiteness = get_pixel(i, 120, 3);//need to check that the first pixel of the picture is gotten when i=0
+                int whiteness = get_pixel(i, 120, 3);//gets a line of pixels halfway down the image
                 if (whiteness>140) { //Changeable, 140 seems to be best number. Might have to be changed depending on lighting
                     whiteness = 1;
                 }
@@ -32,7 +32,7 @@ int main()
             }
             int num = -160;
             for (int i=0; i<320;i++){        //this loop goes through the pixel array and multiplies each value by num, num starts at -160 and increases by 1 each loop       
-                if(num==0){num++;}//addded an "if" to skip 0 so that it goes ....-2,-1,1,2....
+                if(num==0){num++;}           //addded an "if" to skip 0 so that it goes ....-2,-1,1,2....
                 pixels[i]=pixels[i]*num;
                 num++;
             }
@@ -40,12 +40,19 @@ int main()
             for (int i=0; i<320;i++){     //this loop simply adds up the array          
                 sum = sum + pixels[i];
             }
-            /*This code will make the robot go backward if it loses the white line,
-            next step will be to make it turn slightly as its going backward so depending on wether it lost 
-            the line to the right or the left, otherwise it gets stuck going back and forward*/
-            if(sum==0){
-              set_motor(1, -45);
-              set_motor(2, -45);
+            
+            if (sum>0){flip=true;}//if sum = 0 then flip will not be changed and stay whatever it was right before the sum was 0
+            if(sum<0){flip=false;}
+            
+            //The following code will make the robot go backward if it loses the white line havent quite tested but seems to work
+            if(sum==0 && flip == false){
+              set_motor(1, -40);
+              set_motor(2, -50);
+              printf("moving back");
+            }
+            else if(sum==0 && flip == true){
+              set_motor(1, -50);
+              set_motor(2, -40);
               printf("moving back");
             }
             /*if there are lots of white pixels on the left, sum should be very negative, lots on the right and sum should be very positive, 
