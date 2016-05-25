b976f98ce79065cdf08b725440af5abb02aba488
Lenton <lentonneale@gmail.com>
2016-05-25 04:30:02 +0000

    it works

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
+#include <chrono>
 
 extern "C" int init(int d_level);
 extern "C" int Sleep(int sec, int usec);
@@ -14,15 +15,16 @@ extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
 extern "C" int read_analog(int ch_adc);
-extern "C" bool run_sensor=true;
-
+extern "C" int set_pixel(int row, int col, char red, char green, char blue);
 
 using namespace std;
+using namespace std::chrono;
 
 const int white_value = 110;
-const int camera_y_coord = 160;
-const int min_line_width = 10;
-const float speed = 0.8;
+const int camera_y_coord = 0;
+const int min_line_width = 40;
+const float speed = 0.9;
+bool run_sensor = true;
 //Set PID in different error methods
 float P = 0;
 float I = 0;
@@ -40,52 +42,11 @@ int clamp(int a, int b, int val){
     return val;
 }
 
-int error_method_1(){
-    //PID Constants for method 1
-    P = 0.05;
-    I = 0.001;
-    D = 0.0;
-    max_error = 10000;
-
-    int error = 0;
-    for (int i = 0;i<320;i++) {
-        int value = get_pixel(i, camera_y_coord, 3);
-        if (value>white_value) {
-            error += i-160;
-        }
-    }
-    return error;
-}
-
-int error_method_2(){
-    //PID Constants for method 2
-    P = 0.5;
-    I = 0.0;
-    D = 0.0;
-    max_error = 160;
-
-    int left = 0;
-    for (int i = 0;i<160;i++) {
-        int value = get_pixel(i, camera_y_coord, 3);
-        if (value>white_value) {
-            left = 160 - i;
-        }
-    }
-    int right = 0;
-    for (int i = 320;i>=160;i--) {
-        int value = get_pixel(i, camera_y_coord, 3);
-        if (value>white_value) {
-            right = i-160;
-        }
-    }
-    return right - left;
-}
-
 int error_method_3(){
     //PID Constants for method 3
-    P = 0.8;
+    P = 0.3;
     I = 0.0;
-    D = 0.0;
+    D = 50;
     max_error = 160;
 
     int left = 0;
@@ -117,6 +78,61 @@ int error_method_3(){
     return right - left;
 }
 
+int error_method_4(){
+P = 0.28;
+I = 0.0;
+D = 50;
+int left = 0;
+int consec_white_pixels = 0;
+for(int i = 0; i < 320; i++){
+int value = get_pixel(i, camera_y_coord, 3);
+if(value>white_value){
+consec_white_pixels += 1;
+if(consec_white_pixels >= min_line_width){
+left = i - consec_white_pixels;
+break;
+}
+}
+else {
+consec_white_pixels = 0;
+}
+}
+int right = 0;
+consec_white_pixels = 0;
+for(int i = 320; i >=0; i--){
+int value = get_pixel(i, camera_y_coord, 3);
+if(value>white_value){
+consec_white_pixels += 1;
+if(consec_white_pixels>=min_line_width){
+right = i + consec_white_pixels;
+break;
+}
+} else {
+consec_white_pixels = 0;
+}
+}
+return ((left + right)/2)-160;
+}
+
+long get_time(){
+return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+void print_picture(){
+cout << endl;
+for(int i = 0; i < 240; i++){
+for(int j = 0; j < 320; j++){
+int value = get_pixel(j, i, 3);
+if(value > white_value){
+set_pixel(j, i, 255, 255, 255);
+} else {
+set_pixel(j, i, 0, 0, 0);
+}
+}
+}
+update_screen();
+}
+
 bool has_lost_line(){
 	int consec_white_pixels = 0;
     for (int i = 0;i<320;i++) {
@@ -136,22 +152,20 @@ bool has_lost_line(){
 //Initial values
 int integral = 0;
 int last_error = 0;
+long last_loop_end_time = 0;
 void follow_loop(){
             take_picture();
-            update_screen();
-
-            //Remember to change last_error when changing the method
-            int error = error_method_3();
+            print_picture();
+            int error = error_method_4();
+            cout << "has lost line " << has_lost_line() << endl;
             if(has_lost_line()){
-                if(last_error > 0){
-                    error = max_error;
-                } else if(last_error < 0){
-                    error = 0 - max_error;
-                }
+                error = last_error;
             }
             integral += error;
-            int derivitive = last_error - error;
-            last_error = error;
+            long time = get_time();
+            float derivitive = ((float)(error - last_error))/((float)(time - last_loop_end_time));
+            last_loop_end_time = time;
+last_error = error;
             int final_signal = P*error + I*integral + D*derivitive;
 
             cout << "error " << error << endl;
@@ -159,8 +173,8 @@ void follow_loop(){
             cout << "derivitive " << derivitive << endl;
             cout << "final " << final_signal << endl;
 
-            set_motor(1 , speed*clamp(-100, 100, 100+final_signal));
-            set_motor(2 , speed*clamp(-100, 100, 100-final_signal));
+            set_motor(1 , speed*clamp(-100, 150, 50+final_signal));
+            set_motor(2 , speed*clamp(-100, 150, 50-final_signal));
 }
 
 int main()
@@ -175,16 +189,16 @@ int main()
 //    send_to_server(pswd);
     
     int adc_reading = 0;
-    last_error = error_method_3();
-    
+    last_error = error_method_4();
+    last_loop_end_time = get_time();
 	while(true){
   	    adc_reading = read_analog(0);
-	    printf("%d\n", adc_reading);
-		if(adc_reading<100){
+		if(adc_reading<400){
 			follow_loop();
 		} else {
 			set_motor(1 , 0);
             set_motor(2 , 0);
+last_loop_end_time = get_time();
 		}
 	}
     return 0;
